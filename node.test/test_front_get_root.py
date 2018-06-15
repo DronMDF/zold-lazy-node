e30b3618038c95b6addc39191dad31c6c6e97bc8
Andrey Valyaev <dron.valyaev@gmail.com>
2018-06-15 16:14:19 +0300

    #9: Ошибки в стиле

--- node.test/test_front_get_root.py
+++ node.test/test_front_get_root.py
@@ -1,4 +1,11 @@
-import pytest
+# Copyright (c) 2018 Andrey Valyaev <dron.valyaev@gmail.com>
+# Copyright (c) 2018 Alexey Tsurkan
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+''' Тестирование корневого урла '''
+
 from node.app import APP
 
 
