9d87901ee63b2b15d632a08186c5cabb83670a09
Andrey Valyaev <dron.valyaev@gmail.com>
2018-09-09 21:52:58 +0300

    #133: Инсталляция uwsgi вместо gunicorn

---
+++ node/wsgi.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2018 Andrey Valyaev <dron.valyaev@gmail.com>
+# Copyright (c) 2018 Alexey Tsurkan
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+''' WSGI entry '''
+
+from app import APP
+
+
+if __name__ == "__main__":
+	APP.run()
